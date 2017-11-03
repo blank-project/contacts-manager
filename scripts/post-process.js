4d1c1d1b4f15ba6647dfe9d79e2ed2443b6be231
Erwan Osouf <erwan.osouf@gmail.com>
2017-11-03 17:46:35 +0100

    Fix #48

--- scripts/post-process.js
+++ scripts/post-process.js
@@ -2,12 +2,19 @@
 var source = db['contacts-import']
   , target = db.contacts
   , imports = db.imports
+  , tags = {}
   , count = 0
   , currentImport = { meta : { date : new Date() }}
   , importId;
 
 importId = imports.insertOne(currentImport).insertedId;
 currentImport._id = importId;
+
+// Builds a tag lookup map.
+db.tags.find({}).forEach(tag => {
+  var id = tag._id;
+  tags[tag.name.toLowerCase()] = id;
+});
 
 source.find({}).forEach(r => {
   // Transforms imported objects into arrays.
@@ -27,11 +34,30 @@ source.find({}).forEach(r => {
   if (r.addresses && r.addresses['0']) {
     r.addresses = [r.addresses['0']];
   }
+
+  // Lookup Tags
+  if (r.tags) {
+    var targetTags = [];
+    const maxTags = 5;
+    for (var i = 0; i < maxTags; i++) {
+      if (!r.tags[i]) {
+        continue;
+      }
+      var tagName = r.tags[i];
+      tagName = tagName.toLowerCase();
+      if (tags[tagName]) {
+        targetTags.push(tags[tagName])
+      }
+    }
+    r.tags = targetTags;
+  }
   // Set meta fields.
   r.meta = {};
   r.meta.creationDate = new Date();
   r.meta.modificationDate = new Date();
   r.meta.importId = importId;
+
+  // Removes the id from the contacts-import table.
   delete r._id;
   target.save(r);
   count++;
